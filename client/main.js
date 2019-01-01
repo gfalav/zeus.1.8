6cf81023b06b2d175e48638e09da4e4e6931ceb7
Gustavo Falavigna <gfalav@hotmail.com>
2019-01-01 00:40:54 -0300

    topbar error fixed

--- client/main.js
+++ client/main.js
@@ -1,8 +1,8 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/custom.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 import './main.html';
 import './layout/topbar.html';
