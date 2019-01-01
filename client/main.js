795ba2c9a5817a2a40c9294a45e43b5b59202cbd
Gustavo Falavigna <gfalav@hotmail.com>
2019-01-01 12:45:26 -0300

    loginbuttons style fixed

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/custom.css';
 
