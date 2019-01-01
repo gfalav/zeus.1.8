bd8861ec20e73cf39eb3cd5fad69ec427269e618
Gustavo Falavigna <gfalav@hotmail.com>
2019-01-01 19:35:13 -0300

    routes addedd

--- client/main.js
+++ client/main.js
@@ -1,11 +1,12 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
-
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/custom.css';
 
 import './main.html';
 import './layout/topbar.html';
 import './layout/home.html';
 import './layout/footer.html';
+import '/lib/routes.js';
+
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/custom.css';
